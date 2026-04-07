062afdaadf065790195956b06884e4db820f3974
csraynardt <80880816+csraynardt@users.noreply.github.com>
2026-04-07 11:26:20 +0800

    add cloudfront_function variable and function_association blocks

--- _variables.tf
+++ _variables.tf
@@ -142,6 +142,11 @@ variable "lambda_edge" {
   description = "Lambda EDGE configuration"
 }
 
+variable "cloudfront_function" {
+  default     = []
+  description = "CloudFront Function configuration"
+}
+
 variable "default_threshold" {
   description = "The default threshold for the metric."
   default     = 5
